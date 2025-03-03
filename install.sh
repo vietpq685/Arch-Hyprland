6f2f4062bc5aa6ced1a3194f51032c13b09b6fdf
Viet Phung <phungquocviet0608@gmail.com>
2025-03-03 23:28:07 +0700

    add install.sh file

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+echo "WELCOME! Now we will install and setup Hyprland on an Arch-based system"
+echo "Create by VietPQ"
+
+sudo pacman -Syu --noconfirm
+
+cd ~
+
+echo "Install yay"
+sudo pacman -S --noconfirm --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ~
+rm -rf ~/yay
+
+echo "Install Hyprland and necessary components"
+sudo pacman -S --noconfirm hyprland ghostty nemo
+
+echo "Stow dotfiles"
+mkdir -p ~/dotfiles && mv ~/Arch-Hyprland/dotfiles/* ~/dotfiles/
+cd ~/dotfiles
+stow ghostty hypr rofi wallpaper waybar wlogout
+cd ~
+
+echo "Have a great time with Hyprland!!!"
